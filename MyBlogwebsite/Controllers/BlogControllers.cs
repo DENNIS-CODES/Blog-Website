d576a3bc0169c6388a8532784075ada25c9339b6
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-28 00:49:56 +0300

    first commit

---
+++ MyBlogwebsite/Controllers/BlogControllers.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.linq;
+using System.web;
+using System.web.mvc;
+
+namespace Blog_Website_MVC.Controllers
+{
+    public class BLogController : Controller
+    {
+        // GET: Blog
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult Uploading()
+    }
+}
---
+++ MyBlogwebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyBlogwebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
